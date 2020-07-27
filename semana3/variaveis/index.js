494e3480d28894798b3fe5e3b397e4efcf724e98
Magali da Silva <63214096+dsmag@users.noreply.github.com>
2020-07-27 14:36:05 -0300

    Respostas do exercício da aula sobre variáveis

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,82 @@
+/* EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO */
+
+// 1. No primeiro console.log será impresso o valor 10. No segundo console.log será impresso o valor 10 e 5.
+
+// 2. Será impresso no console: 10, 10, 10.
+
+
+
+
+
+/* EXERCÍCIOS DE ESCRITA DE CÓDIGO */
+
+/* 1 */
+// let nome
+// let idade
+
+// console.log(typeof(nome, idade))
+
+// retorna "undefined" porque as variáveis não receberam valores, consequentemente não possuem um tipo.
+
+// let nome = prompt("Qual o seu nome?")
+// let idade = prompt("Qual a sua idade?")
+
+// console.log(typeof(nome, idade)) // ambas retornam tipo string
+
+// console.log("Olá", nome,", você tem", idade, "anos." )
+// console.log(`Olá ${nome}, você tem ${idade} anos.`)
+
+
+
+/* 2 */
+// let nome = prompt("Qual o seu nome?")
+// let idade = prompt("Qual a sua idade?") 
+// let endereco = prompt("Qual o seu endereço?") 
+// let nacionalidade = prompt("Qual a sua nacionalidade?") 
+// let corFavorita = prompt("Qual a sua cor favorita?") 
+
+// console.log("1. Qual o seu nome?")
+// console.log("Resposta:", nome)
+
+// console.log("2. Qual a sua idade?")
+// console.log("Resposta:", idade)
+
+// console.log("3. Qual o seu endereço?")
+// console.log("Resposta:", endereco)
+
+// console.log("4. Qual a sua nacionalidade?")
+// console.log("Resposta:", nacionalidade)
+
+// console.log("5. Qual a sua cor favorita?")
+// console.log("Resposta:", corFavorita)
+
+
+
+/* 3 */
+// let comidasPreferidas = ["Lasanha", "Pizza", "Feijão", "Massa carbonara", "Churrasco"]
+
+// console.log(comidasPreferidas)
+
+// console.log("Essas são as minhas comidas preferidas:")
+// console.log(comidasPreferidas[0])
+// console.log(comidasPreferidas[1])
+// console.log(comidasPreferidas[2])
+// console.log(comidasPreferidas[3])
+// console.log(comidasPreferidas[4])
+
+// let pergunta = prompt("Qual a sua comida favorita?")
+
+// comidasPreferidas[1] = pergunta;
+
+// console.log(comidasPreferidas) 
+
+
+
+/* 4 */
+let perguntas = ["Você está usando uma roupa azul hoje?", "Você tomou água hoje?", "Você comeu fruta hoje?"]
+
+let respostas = [false, true, false]
+
+console.log(perguntas[0], respostas[0])
+console.log(perguntas[1], respostas[1])
+console.log(perguntas[2], respostas[2])
