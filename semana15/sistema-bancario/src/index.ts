2622d923074dded66b3ba7eebc728cb04de2346e
Magali da Silva <63214096+dsmag@users.noreply.github.com>
2020-10-23 16:53:22 -0300

    métodos get e post, incompleto

--- semana15/sistema-bancario/src/index.ts
+++ semana15/sistema-bancario/src/index.ts
@@ -1,14 +1,46 @@
-import express, {  Express } from 'express'
+import express, {  Express, Request, Response } from 'express'
 import cors from 'cors'
-import { Extract, userAccount, users } from './users'
+import { userAccount, users } from './users'
 
 const app: Express = express()
 
 app.use(express.json())
 app.use(cors())
 
+app.get("/users", (req: Request, res: Response): void => {
+    try {
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(400).send({ message: "Error searching for users" });
+    }
+})
 
+app.post("/users", (req: Request, res: Response): void => {
+    try {
+        const { id, name, cpf, dateOfBirth, balance, value, date, description } = req.body;
 
+        const user: userAccount = {
+            id: id,
+            name: name,
+            cpf: cpf,
+            dateOfBirth: dateOfBirth,
+            balance: balance,
+            extract: [
+                {
+                    value: value,
+                    date: date,
+                    description: description
+                }
+            ]
+        }
+
+        users.push(user)
+
+        res.status(200).send({message: "User created successfully" });
+    } catch (error) {
+        res.status(400).send({ message: "Error inserting users" });
+    }
+})
 
 
 app.listen(3003, () => {
